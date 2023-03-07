c99017521b582b875a990e0e43ad081129a4ba20
Umberto Mahieu <umbertomahieu@gmail.com>
2023-03-07 10:11:27 +0100

    solution created as model and structure

---
+++ MonkeyTinder/MonkeyTinder/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MonkeyTinder;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MonkeyTinder/MonkeyTinder/Model/Monkey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyTinder.Model
+{
+    internal class Monkey
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        public string Image { get; set; }
+        public int Bonnet { get; set; }
+        public string Situation { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
